f3b02ffedcdcc5dc3ec9d0abaab3ccb13f239b22
Sardor Davlenov <sdavlenov@gmail.com>
2015-09-11 21:36:43 +0000

    added the 4 variables with values

---
+++ task_12.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import decimal
+import fractions
+
+INTVAL = 1
+FLOATVAL = 0.1
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
