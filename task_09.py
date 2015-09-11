2c6eaa1521accd87c0b5a82a173bb03f863d45ca
Sardor Davlenov <sdavlenov@gmail.com>
2015-09-11 20:38:49 +0000

    docstring with TWO paragraphs

--- task_09.py
+++ task_09.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.  This is a really long docstring.
+   This is a really long doctring.  This is a really long docstring.  This is a
+   really long docstring.  This is a really long docstring.  This is a really
+   long docstring.  This is a really long docstring.  This is a really long
+   docstring.
+
+   This is the second paragraph of the docstring.  This is the second paragraph
+   of the docstring.  This is the second paragraph of the docstring.  This is
+   the second paragraph of the docstring. This is the second paragraph of the
+   docstring."""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
